fabddc3fcc5f6a8397090ebfc6a5b5a56a53d75b
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-08 09:19:32 +0200

    Export IMM baseline tracker

--- src/raft_uav/baselines/__init__.py
+++ src/raft_uav/baselines/__init__.py
@@ -1,5 +1,13 @@
 """Baseline trackers."""
 
+from raft_uav.baselines.imm import (
+    AsyncInteractingMultipleModelTracker,
+    IMMMode,
+    default_imm_modes,
+    fixed_turn_rate_matrix,
+    run_async_imm_baseline,
+    uniform_ctmc_transition_matrix,
+)
 from raft_uav.baselines.radar_association import (
     RADAR_ASSOCIATION_MODES,
     run_async_cv_baseline_with_radar_association,
@@ -7,8 +15,14 @@ from raft_uav.baselines.radar_association import (
 from raft_uav.baselines.smoothing import SMOOTHER_MODES, smooth_tracking_records
 
 __all__ = [
+    "AsyncInteractingMultipleModelTracker",
+    "IMMMode",
     "RADAR_ASSOCIATION_MODES",
     "SMOOTHER_MODES",
+    "default_imm_modes",
+    "fixed_turn_rate_matrix",
     "run_async_cv_baseline_with_radar_association",
+    "run_async_imm_baseline",
     "smooth_tracking_records",
+    "uniform_ctmc_transition_matrix",
 ]
