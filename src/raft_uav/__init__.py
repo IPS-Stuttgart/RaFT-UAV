1033edd63e75575db3b6a4d30c4c786d044f6965
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-05-18 12:47:59 +0200

    Enable runtime CLI reproducibility shim

--- src/raft_uav/__init__.py
+++ src/raft_uav/__init__.py
@@ -2,6 +2,7 @@
 
 from raft_uav.baselines.radar_covariance_runtime import install as _install_radar_covariance
 from raft_uav.baselines.tracklet_viterbi_runtime import install as _install_tracklet_viterbi
+from raft_uav.runtime_cli_patch import install as _install_runtime_cli_patch
 
 __all__ = ["__version__"]
 
@@ -9,3 +10,4 @@ __version__ = "0.1.0"
 
 _install_radar_covariance()
 _install_tracklet_viterbi()
+_install_runtime_cli_patch()
