83227f4bc792673ddcb3c2954015614ede5e89c3
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-06 17:28:21 +0200

    Use pandas records in numeric ID regression

--- tests/test_mmuad_numeric_ids_placeholder.py
+++ tests/test_mmuad_numeric_ids_placeholder.py
@@ -6,5 +6,5 @@ from raft_uav.mmuad.evaluate import match_submission_to_truth
 
 
 def test_numeric_id_placeholder() -> None:
-    values = [1.0, 2.0]
-    assert values == [1.0, 2.0]
+    frame = pd.DataFrame.from_records([{"id": 1.0}, {"id": 2.0}])
+    assert frame["id"].tolist() == [1.0, 2.0]
