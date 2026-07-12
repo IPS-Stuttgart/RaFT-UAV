9a4a564c312173d6cc93d8146c8a7040cb34d901
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-12 03:13:02 +0200

    Add spatial posterior-mass MMUAD script wrapper

---
+++ scripts/mmuad_candidate_mixture_group_spatial_mass_topk.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Run posterior-mass-correct spatial MMUAD group selection."""
+
+from raft_uav.mmuad.candidate_mixture_group_spatial_mass_topk import main
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
