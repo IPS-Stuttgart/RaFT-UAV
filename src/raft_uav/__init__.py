abe391ee6b3acf1799e589ddf03874acab7dbbb3
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-27 01:55:58 +0200

    Install class probability label validation

--- src/raft_uav/__init__.py
+++ src/raft_uav/__init__.py
@@ -95,6 +95,12 @@ def _repair_complex_row_validation_install() -> Callable[[], None]:
     return install
 
 
+def _class_probability_label_validation_install() -> Callable[[], None]:
+    from raft_uav.mmuad._class_probability_label_validation_patch import install
+
+    return install
+
+
 if os.environ.get("RAFT_UAV_SKIP_RUNTIME_HOOKS") != "1":
     _optional_runtime_hook(_radar_covariance_install)
     _optional_runtime_hook(_tracklet_viterbi_install)
@@ -110,3 +116,4 @@ _optional_runtime_hook(_uncertainty_payload_validation_install)
 _optional_runtime_hook(_candidate_reservoir_config_validation_install)
 _optional_runtime_hook(_acceleration_limit_displacement_install)
 _optional_runtime_hook(_repair_complex_row_validation_install)
+_optional_runtime_hook(_class_probability_label_validation_install)
