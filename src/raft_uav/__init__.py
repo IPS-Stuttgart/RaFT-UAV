03e3108bda590a142689c18bbd8d4b64b2538f29
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-26 16:14:17 +0200

    Install exact reservoir schema validation

--- src/raft_uav/__init__.py
+++ src/raft_uav/__init__.py
@@ -63,6 +63,12 @@ def _uncertainty_payload_validation_install() -> Callable[[], None]:
     return install
 
 
+def _candidate_reservoir_config_validation_install() -> Callable[[], None]:
+    from raft_uav.mmuad._candidate_reservoir_config_validation_patch import install
+
+    return install
+
+
 if os.environ.get("RAFT_UAV_SKIP_RUNTIME_HOOKS") != "1":
     _optional_runtime_hook(_radar_covariance_install)
     _optional_runtime_hook(_tracklet_viterbi_install)
@@ -73,3 +79,4 @@ if os.environ.get("RAFT_UAV_SKIP_RUNTIME_HOOKS") != "1":
 _optional_runtime_hook(_kalman_timestamp_validation_install)
 _optional_runtime_hook(_rf_measurement_fallback_install)
 _optional_runtime_hook(_uncertainty_payload_validation_install)
+_optional_runtime_hook(_candidate_reservoir_config_validation_install)
