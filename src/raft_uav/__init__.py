7416feedaa4e9cbc7278a71f4617a8499d945c71
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-07-27 08:20:20 +0200

    Install strict MOT match-distance validation

--- src/raft_uav/__init__.py
+++ src/raft_uav/__init__.py
@@ -107,6 +107,12 @@ def _acceleration_sequence_id_install() -> Callable[[], None]:
     return install
 
 
+def _mot_match_distance_complex_install() -> Callable[[], None]:
+    from raft_uav.mmuad._mot_match_distance_complex_patch import install
+
+    return install
+
+
 def _candidate_pull_index_install() -> Callable[[], None]:
     from raft_uav.mmuad._candidate_pull_index_patch import install
 
@@ -130,4 +136,5 @@ _optional_runtime_hook(_candidate_reservoir_config_validation_install)
 _optional_runtime_hook(_acceleration_limit_displacement_install)
 _optional_runtime_hook(_repair_complex_row_validation_install)
 _optional_runtime_hook(_acceleration_sequence_id_install)
+_optional_runtime_hook(_mot_match_distance_complex_install)
 _optional_runtime_hook(_candidate_pull_index_install)
