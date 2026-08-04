50f2d54be42122d56e5fa34e7cc961d9ba7d1be0
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-08-04 16:08:39 +0800

    Enable numeric stress output ordering

--- src/raft_uav/stress/__init__.py
+++ src/raft_uav/stress/__init__.py
@@ -1,3 +1,4 @@
 """Stress-test perturbation helpers for RaFT-UAV artifacts."""
 
 from . import _perturbation_boolean_flag_patch as _perturbation_boolean_flag_patch
+from . import _perturbation_numeric_order_patch as _perturbation_numeric_order_patch
