1e92ff4746e62319328af11ce6ba497b5a85e90a
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-08-05 13:48:36 +0800

    Install physical-frame stress grouping

--- src/raft_uav/stress/__init__.py
+++ src/raft_uav/stress/__init__.py
@@ -1,3 +1,4 @@
 """Stress-test perturbation helpers for RaFT-UAV artifacts."""
 
+from . import _perturbation_frame_group_patch as _perturbation_frame_group_patch
 from . import _perturbation_boolean_flag_patch as _perturbation_boolean_flag_patch
