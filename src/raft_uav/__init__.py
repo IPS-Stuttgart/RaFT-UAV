5a3e446633caf3a16edeeb9716912089d8b970ba
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-08-07 02:20:44 +0800

    refactor(evaluator): keep guard in maintained wrapper

--- src/raft_uav/__init__.py
+++ src/raft_uav/__init__.py
@@ -209,12 +209,6 @@ def _track5_estimate_calibration_duplicate_truth_install() -> Callable[[], None]
     return install
 
 
-def _evaluator_header_validation_install() -> Callable[[], None]:
-    from raft_uav.mmuad._evaluator_header_validation_patch import install
-
-    return install
-
-
 if os.environ.get("RAFT_UAV_SKIP_RUNTIME_HOOKS") != "1":
     _optional_runtime_hook(_radar_covariance_install)
     _optional_runtime_hook(_tracklet_viterbi_install)
@@ -249,4 +243,3 @@ _optional_runtime_hook(_track5_rts_template_validation_install)
 _optional_runtime_hook(_candidate_score_calibration_order_install)
 _optional_runtime_hook(_track5_submission_schema_guard_install)
 _optional_runtime_hook(_track5_estimate_calibration_duplicate_truth_install)
-_optional_runtime_hook(_evaluator_header_validation_install)
